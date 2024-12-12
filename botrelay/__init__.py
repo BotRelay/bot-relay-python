0acaf204e1b5f9595dbfd3b71ad2f4f6d6963a76
arungeorgesaji <arungeorgesaji@gmail.com>
2024-12-12 11:17:28 +0530

    Made the basic folder structure and added all the __init__.py

---
+++ botrelay/__init__.py
@@ -0,0 +1,11 @@
+from .core import *
+from .chat import *
+from .platform_integrations import *
+
+__all__ = [
+    "core",
+    "chat",
+    "platform_integrations",
+]
+
+__version__ = "0.1.0"
--- botrelay/core.py
+++
@@ -1,5 +0,0 @@
-from .core import *
-
-__all__ = ["send_message", "receive_message"]
-
-__version__ = "0.1.0"
